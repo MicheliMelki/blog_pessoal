965e8432ed6530785da29566662fd5101e6d3498
Micheli Melki <michelimelki@gmail.com>
2025-07-29 17:07:45 -0300

    Adicionando elementos

---
+++ src/postagem/controller/postagem.controller.ts
@@ -0,0 +1,42 @@
+import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from './../services/postagem.service';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+
+@Controller("/postagens")
+export class PostagemController {
+  postagemRepository: any;
+  constructor(private readonly PostagemService: PostagemService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Postagem[]> {
+    return this.PostagemService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.PostagemService.findById(id);
+  }
+
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.PostagemService.findAllByTitulo(titulo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() postagem: Postagem) : Promise<Postagem>{
+    return this.PostagemService.create(postagem)
+  }
+
+  
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number){
+    return this.PostagemService.delete(id)
+  }
+
+
+}
